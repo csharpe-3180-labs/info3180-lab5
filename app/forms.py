99a17e19a050b2d3f8bd81657d29dbf73d82f129
Carl Sharpe <carlsharpe87@gmail.com>
2026-04-13 11:32:27 -0500

    Updates to base code

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,17 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+
+class MovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    poster = FileField('Movie Poster', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'jpeg', 'png', 'gif', 'webp'], 'Images only!')
+    ])
--- app/models.py
+++ app/models.py
@@ -1,1 +1,56 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+class UserProfile(db.Model):
+    __tablename__ = 'user_profiles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(256))
+
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+# Helper function to update password for a user
+def update_user_password(username, new_password):
+    user = UserProfile.query.filter_by(username=username).first()
+    if user:
+        user.password = generate_password_hash(new_password)
+        db.session.commit()
+        return True
+    return False
+
+class Movie(db.Model):
+	__tablename__ = 'movies'
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(255), nullable=False)
+	description = db.Column(db.Text, nullable=False)
+	poster = db.Column(db.String(255), nullable=False)
+	created_at = db.Column(db.DateTime, default=datetime.utcnow)
